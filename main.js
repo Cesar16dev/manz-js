d27bdaf708916985dea097f8c91c235dc306c942
Cesar Ortega <cesarluis4056@gmail.com>
2024-03-19 11:25:36 -0500

    agregando style.css

---
+++ main.js
@@ -0,0 +1,20 @@
+//Qué son los objetos?
+const objeto = {}; // Esto es un objeto vacío
+
+const player = {
+  //key:value
+  name: "Manz",
+  life: 99,
+  power: 10,
+};
+
+console.log(player.power);
+console.log(player["name"]);
+
+const person = {};
+
+person.name = "Cesar";
+person.age = 19;
+person.power = 30;
+
+console.log(person.name);
