5912b7e35e66d147001777f7fbcce4098bf7d518
Cesar Ortega <cesarluis4056@gmail.com>
2024-03-25 16:20:39 -0500

    Practice methods  arrays

--- js/arrays.js
+++ js/arrays.js
@@ -1,6 +1,13 @@
+//¿Qué es un array?
+//Es una colección o agrupación de elementos en una misma variable
+
 //¿Qué son las Array functions?
 
 const letters = ["a", "b", "c", "d", "e"];
+
+let usingWith = letters.with(1, "B");
+console.log(usingWith);
+console.log(letters);
 
 letters.forEach((letter, index) => {
   //console.log(`Las primeras letras del ABC son : ${letter}`);
